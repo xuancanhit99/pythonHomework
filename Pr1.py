921915eb274c988527a9ddc6b73b4889678d1e75
Xuan Canh <xuancanhit99@gmail.com>
2021-02-15 00:24:47 +0300

    Update full pr1

---
+++ Pr1.py
@@ -0,0 +1,41 @@
+import math
+
+
+def f1(x, y, z):
+    return y**4 - math.e**y - 95 + 27*(x**4 - math.cos(z))**2 + z**3 - (y**3 + 64*x**2 - 32)/(16*y**3 - x**8)
+
+
+def f2(x):
+    if x < 119:
+        return x**5 - math.e**x - 95
+    elif x < 141 & x >= 119:
+        return (25*x**4 + (x**5)/67)**2 + x**8
+    elif x < 161 & x >= 141:
+        return math.cos(x**6 + x/84) + math.sin(x**8)
+    elif x < 178 & x >= 161:
+        return math.cos(38*x**8 - math.e**x) + math.log(math.e**x + math.sin(x) + 76)
+    else:
+        return 97*x + math.e**x
+
+
+def sigma_sum(start, end, expression):
+    return sum(expression(i) for i in range(start, end))
+
+
+def f3(n, m):
+    sum1 = 0
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            sum1 += math.tan(j) - math.tan(i)
+
+    sum2 = 0
+    for i in range(1, n+1):
+        sum2 += math.e**i - 36*i**8 - 27
+
+    return 45*sum1 + 92*sum2
+
+
+def f4(n):
+    if n == 0:
+        return 7
+    return math.cos(f4(n-1)) - math.tan(f4(n-1)) - 41
